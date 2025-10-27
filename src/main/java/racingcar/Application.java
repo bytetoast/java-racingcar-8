9b1112c1364af83f20095f12181713b69f9bf5db
bytetoast <hvagdn@gmail.com>
2025-10-27 19:37:13 +0900

    feat(main): read and validate user input about car names

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,15 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Pattern;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carsStringRaw = Console.readLine();
+        final String NAMES_REGEX = "^[a-zA-Z0-9]{1,5}(,[a-zA-Z0-9]{1,5})*$";
+        if (!Pattern.matches(NAMES_REGEX, carsStringRaw)) {
+            throw new IllegalArgumentException("Invalid car input.");
+        }
     }
 }
