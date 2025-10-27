e62b57ed1e7a5c0a3dd81b73dcf4427cde0d25d7
bytetoast <hvagdn@gmail.com>
2025-10-27 10:59:15 +0900

    feat(racemanager): introduce RaceManager class

---
+++ src/main/java/racingcar/RaceManager.java
@@ -0,0 +1,39 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RaceManager {
+    List<Car> cars;
+
+    public RaceManager() {
+        this.cars = new ArrayList<>();
+    }
+
+    public void addCar(String name) {
+        this.cars.add(new Car(name));
+    }
+
+    public void processRound() {
+        for (Car car : this.cars) {
+            car.move();
+        }
+        System.out.println("\n");
+    }
+
+    public String getWinners() {
+        int max = -1;
+        ArrayList<String> winners = new ArrayList<>();
+        for (Car car : this.cars) {
+            if (car.getPosition() > max) {
+                max = car.getPosition();
+                winners.clear();
+                winners.add(car.getName());
+            } else if (car.getPosition() == max) {
+                winners.add(car.getName());
+            }
+        }
+
+        return String.join(", ", winners);
+    }
+}
